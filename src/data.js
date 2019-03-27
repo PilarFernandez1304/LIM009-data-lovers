3ca8bf7fe00b563d4b3ed8dc042864c9d058681f
PilarFernandez1304 <ccentepilar@gmail.com>
2019-03-27 06:48:09 -0500

    DOM evolucion terminado

--- src/data.js
+++ src/data.js
@@ -11,8 +11,8 @@
 
 /**** MOSTRAR POEKOM ****/
 const mostrarListaPokemon = (dataPoke) => {
-  pokedex = dataPoke.map(data =>{
-    return{num : data.num , img : data.img , name : data.name , avg_spawns : data.avg_spawns};
+  pokedex = dataPoke.map(data => {
+    return { num: data.num, img: data.img, name: data.name, avg_spawns: data.avg_spawns };
   });
   return pokedex;
 }
@@ -20,28 +20,28 @@ const mostrarListaPokemon = (dataPoke) => {
 
 
 /**** ORDENAR POKEMON ****/
-const sortData = (data,sortBy,sortOrder) =>{
-    switch(sortOrder){
-      case 'ascendente':
-        if(sortBy ==='avg_spawns'){
-          listaOrdenada = data.sort((a,b)=> a.avg_spawns - b.avg_spawns);
-        }
-        else if(sortBy ==='az' ){
-          listaOrdenada = data.sort((a,b)=> a.name > b.name ? 1:-1);
-        }            
+const sortData = (data, sortBy, sortOrder) => {
+  switch (sortOrder) {
+    case 'ascendente':
+      if (sortBy === 'avg_spawns') {
+        listaOrdenada = data.sort((a, b) => a.avg_spawns - b.avg_spawns);
+      }
+      else if (sortBy === 'az') {
+        listaOrdenada = data.sort((a, b) => a.name > b.name ? 1 : -1);
+      }
       break;
-      case 'descendente':
-        listaOrdenada = data.sort((a,b)=>b.name > a.name ? 1:-1);
+    case 'descendente':
+      listaOrdenada = data.sort((a, b) => b.name > a.name ? 1 : -1);
       break;
-    }
-    return listaOrdenada;
+  }
+  return listaOrdenada;
 }
 //console.log(sortData(dataPokedex,'az','ascendente'));
 
 /***  FILTRAR POKEMON ***/
-const filterData = (data,condition) =>{
-  listaFiltrada = data.filter((element)=>{
-    for(let i = 0 ; i < element.type.length ; i++){
+const filterData = (data, condition) => {
+  listaFiltrada = data.filter((element) => {
+    for (let i = 0; i < element.type.length; i++) {
       if (element.type[i] === condition) {
         return 1;
       }
@@ -50,135 +50,132 @@ const filterData = (data,condition) =>{
   return listaFiltrada;
 }
 //console.log(filterData(data,'Fire'));
-
-let Evolucion1=[];
-let Evolucion2=[];
-let Evolucion3=[];
-for (let i=0 ; i<POKEMON.pokemon.length ; i++){
-  let element=POKEMON.pokemon[i];
-  if(element.prev_evolution === undefined)
-  {
+let Evolucion1 = [];
+let Evolucion2 = [];
+let Evolucion3 = [];
+for (let i = 0; i < POKEMON.pokemon.length; i++) {
+  let element = POKEMON.pokemon[i];
+  if (element.prev_evolution === undefined) {
     Evolucion1.push(element);
-    Evolucion2=[];
-    Evolucion3=[];
-    
-  
-  }else{ 
-    let cantidadPrev=element.prev_evolution.length
-    switch(cantidadPrev){
+  } else {
+    let cantidadPrev = element.prev_evolution.length
+    switch (cantidadPrev) {
       case 1:
-      Evolucion2.push(element)
-      Evolucion1=[];
-      Evolucion3=[];
-      ;break;
+        Evolucion2.push(element); break;
       case 2:
-      Evolucion3.push(element);
-      Evolucion1=[];
-      Evolucion2=[];
-      break;
+        Evolucion3.push(element); break;
     }
   }
 }
 
+function NivelEvolu(condicion) {
+  switch (condicion) {
+    case "Evolucion1": return Evolucion1; break;
+    case "Evolucion2": return Evolucion2; break;
+    case "Evolucion3": return Evolucion3; break;
+  }
+}
 
-const listType = (data) =>{
- let arrTipos =[];
- let tipo =[];
- data.forEach(element => {
-   for(let i = 0 ; i < element.type.length ; i++){
-     arrTipos.push(element.type[i]);
-   }  
-   tipo = [...new Set(arrTipos)]
-   });
-   return tipo;
+
+
+const listType = (data) => {
+  let arrTipos = [];
+  let tipo = [];
+  data.forEach(element => {
+    for (let i = 0; i < element.type.length; i++) {
+      arrTipos.push(element.type[i]);
+    }
+    tipo = [...new Set(arrTipos)]
+  });
+  return tipo;
 }
 //console.log(tipos(data));
 
 
 /***** CALCULAR POKEMON ******/
 
-const dataListPokemon = (dataPoke) =>{
-  calcularLista =[] 
-  for(let i = 0 ; i < dataPoke.length ; i++  ){
+const dataListPokemon = (dataPoke) => {
+  calcularLista = []
+  for (let i = 0; i < dataPoke.length; i++) {
     propiedad = dataPoke[i].hasOwnProperty('next_evolution');
-    if(propiedad === true ) {
-       propiedad = dataPoke[i].next_evolution.length;
-    } 
-    else{
-      propiedad = 0;
-    }      
-    calcularLista.push( {
-       num : dataPoke[i].num , 
-       name : dataPoke[i].name , 
-       candy_count : dataPoke[i].candy_count ,
-       next_evolution : dataPoke[i].next_evolution,
-       prev_evolution : dataPoke[i].prev_evolution,
-       num_evoluciones : propiedad,
-       img : dataPoke[i].img })
+    if (propiedad === true) {
+      propiedad = dataPoke[i].next_evolution.length;
     }
-    return calcularLista;
+    else {
+      propiedad = 0;
+    }
+    calcularLista.push({
+      num: dataPoke[i].num,
+      name: dataPoke[i].name,
+      candy_count: dataPoke[i].candy_count,
+      next_evolution: dataPoke[i].next_evolution,
+      prev_evolution: dataPoke[i].prev_evolution,
+      num_evoluciones: propiedad,
+      img: dataPoke[i].img
+    })
+  }
+  return calcularLista;
 }
 //console.log(dataListPokemon(data));
 
-const computeStats = (dataPoke,pokemon,dulce) =>{
-
-      calcularLista = dataListPokemon(dataPoke);
-      encontrado = calcularLista.find(poke=>poke.name === pokemon);
-      numeroEvoluciones = encontrado.num_evoluciones;
-      let arrResult = [];
-      let evolucion;
-      let anterior;
-      let calculoEvolution;
-
-      switch (numeroEvoluciones){  
-        // No ha evolucionado aun
-        case 2:
-        if(dulce < encontrado.candy_count){
-          evolucion = calcularLista.find( poke => poke.name === encontrado.next_evolution[0].name );
-          evolucion = evolucion.name;
-          calculoEvolution = encontrado.candy_count - parseInt(dulce);
-        }
-        else{
-          alert('no');
-          evolucion = 'Este pokemon ya evoluciono ';
-          calculoEvolution = 0;
-        }
-        break; 
-        // ya tiene la primera evolucion 
-        case 1:  
-        anterior = calcularLista.find( poke => poke.name === encontrado.prev_evolution[0].name );
-        anterior = anterior.candy_count;
-        
-        if( dulce > anterior  && dulce < encontrado.candy_count ){
-          evolucion = calcularLista.find( poke => poke.name === encontrado.next_evolution[0].name );
-          evolucion = evolucion.name;
-          calculoEvolution = encontrado.candy_count - parseInt(dulce);
-        }
-        else{
-          alert('no');
-          evolucion = 'Este pokemon ya evoluciono';
-          calculoEvolution = 0;
-        }          
-        break;
-        // ya termino de evolucionar
-        case 0:
-        evolucion = 'Este pokemon ya tuvo todas su evoluciones ';
-        calculoEvolution = 0;
-        break;
+const computeStats = (dataPoke, pokemon, dulce) => {
+  calcularLista = dataListPokemon(dataPoke);
+  encontrado = calcularLista.find(poke => poke.name === pokemon);
+  numeroEvoluciones = encontrado.num_evoluciones;
+  let arrResult = [];
+  let evolucion;
+  let anterior;
+  let calculoEvolution;
+  switch (numeroEvoluciones) {
+    // No ha evolucionado aun
+    case 2:
+      if (dulce < encontrado.candy_count) {
+        evolucion = calcularLista.find(poke => poke.name === encontrado.next_evolution[0].name);
+        evolucion = evolucion.name;
+        calculoEvolution = encontrado.candy_count - parseInt(dulce);
       }
-      arrResult.push({evolution : evolucion , candy_evolution : calculoEvolution })
-      return arrResult;
+      else {
+        alert('no');
+        evolucion = 'Este pokemon ya evoluciono ';
+        calculoEvolution = 0;
+      }
+      break;
+    // ya tiene la primera evolucion 
+    case 1:
+      anterior = calcularLista.find(poke => poke.name === encontrado.prev_evolution[0].name);
+      anterior = anterior.candy_count;
+
+      if (dulce > anterior && dulce < encontrado.candy_count) {
+        evolucion = calcularLista.find(poke => poke.name === encontrado.next_evolution[0].name);
+        evolucion = evolucion.name;
+        calculoEvolution = encontrado.candy_count - parseInt(dulce);
+      }
+      else {
+        alert('no');
+        evolucion = 'Este pokemon ya evoluciono';
+        calculoEvolution = 0;
+      }
+      break;
+    // ya termino de evolucionar
+    case 0:
+      evolucion = 'Este pokemon ya tuvo todas su evoluciones ';
+      calculoEvolution = 0;
+      break;
   }
- //console.log(computeStats(data ,"Ivysaur",25 ));
+  arrResult.push({ evolution: evolucion, candy_evolution: calculoEvolution })
+  return arrResult;
+}
+//console.log(computeStats(data ,"Ivysaur",25 ));
 
 
-  
+
 
 window.pokemon = {
   mostrarListaPokemon: mostrarListaPokemon,
-  sortData : sortData,
-  filterData : filterData,
-  listType : listType,
-  dataListPokemon :dataListPokemon,
-  computeStats :computeStats
+  sortData: sortData,
+  filterData: filterData,
+  NivelEvolu: NivelEvolu,
+  listType: listType,
+  dataListPokemon: dataListPokemon,
+  computeStats: computeStats
 }
--- src/main.js
+++ src/main.js
@@ -7,121 +7,127 @@ const pageEvolution = document.getElementById("page-evolution");
 const pageNews = document.getElementById("page-news");
 
 
-main.addEventListener("click",(e)=>{
+main.addEventListener("click", (e) => {
     pageHome.style.display = 'none';
     pagePokedex.style.display = 'none';
     pageEvolution.style.display = 'none';
     pageNews.style.display = 'none';
 
-    if(e.target.id==="pokedex"){
-        pagePokedex.style.display='block';
-        document.getElementById('list-pokemon').innerHTML= crearPlantilla(data);
+    if (e.target.id === "pokedex") {
+        pagePokedex.style.display = 'block';
+        document.getElementById('list-pokemon').innerHTML = crearPlantilla(data);
     }
-    else if(e.target.id==="evolution"){
-        pageEvolution.style.display='block';        
+    else if (e.target.id === "evolution") {
+        pageEvolution.style.display = 'block';
     }
-    else if(e.target.id==="news"){
-        pageNews.style.display='block';
+    else if (e.target.id === "news") {
+        pageNews.style.display = 'block';
     }
-    else{
-        pageHome.style.display='block';
+    else {
+        pageHome.style.display = 'block';
     }
 });
 
 /*2.FUNCIONES */
 
 const data = POKEMON.pokemon;
-const dataPokedex = pokemon.mostrarListaPokemon(data); 
+const dataPokedex = pokemon.mostrarListaPokemon(data);
 
 // MOSTRAR POKEMON 
 const crearPlantilla = (data) => {
     let listaMostrada = [];
-    data.forEach((data)=>{
-        let card =`
+    data.forEach((data) => {
+        let card = `
         <div class="card-item grow">
         <h1>N° ${data.num}</h1>
         <img class="img-res" src='${data.img}'>
         <p>${data.name}</p>
         </div>
         `;
-        listaMostrada +=card;
+        listaMostrada += card;
     });
     return listaMostrada;
 };
 
 // ORDENAR POKEMON
 const orderPokemon = document.getElementById('order-pokemon');
-orderPokemon.addEventListener('change',()=>{
+orderPokemon.addEventListener('change', () => {
     const selectOrder = orderPokemon.value;
     let sortOrder;
-    switch(selectOrder){
+    switch (selectOrder) {
         case 'avg_spawns':
-        sortOrder = 'ascendente';
-        break;
+            sortOrder = 'ascendente';
+            break;
         case 'az':
-        sortOrder ='ascendente';
-        break;
+            sortOrder = 'ascendente';
+            break;
         case 'za':
-        sortOrder = 'descendente';
-        break;
+            sortOrder = 'descendente';
+            break;
     }
-    const pokedexOrdenado = pokemon.sortData(dataPokedex,selectOrder,sortOrder);
-    document.getElementById('list-pokemon').innerHTML= crearPlantilla(pokedexOrdenado);
+    const pokedexOrdenado = pokemon.sortData(dataPokedex, selectOrder, sortOrder);
+    document.getElementById('list-pokemon').innerHTML = crearPlantilla(pokedexOrdenado);
 });
 
 // FILTRAR POKEMON
-const FilterSelect= document.getElementById('FilterSelect')
-FilterSelect.addEventListener('change',SelectFilter);
-let array; 
-function SelectFilter(){
- const a = FilterSelect.value;
-    if(a==='1'){
-          array=["Grass","Poison","Fire","Flying","Water",
-          "Bug","Normal","Electric","Ground","Fighting",
-          "Psychic","Rock","Ice","Ghost","Dragon"];
-          
- 
-    
-    }else if(a ==="2"){
-         array=['Evolucion1','Evolucion2','Evolucion3'];
-    }else{
-        
-         array=[];
+const FilterSelect = document.getElementById('FilterSelect')
+FilterSelect.addEventListener('change', SelectFilter);
+let array;
+function SelectFilter() {
+    const a = FilterSelect.value;
+    if (a === '1') {
+        array = ["Grass", "Poison", "Fire", "Flying", "Water",
+            "Bug", "Normal", "Electric", "Ground", "Fighting",
+            "Psychic", "Rock", "Ice", "Ghost", "Dragon"];
+
+
+
+    } else if (a === "2") {
+        array = ['Evolucion1', 'Evolucion2', 'Evolucion3'];
+    } else {
+
+        array = [];
     }
-    let string='';
-   
-    for (let i = 0; i<array.length; i++) {
-      let eleccion=array[i];
+    let string = '';
 
-     string =string+ '<option values="'+eleccion+'">'+eleccion+'</option>';        
-    } 
-    string='<select id="filter">'+string+'</select>';
-    document.getElementById('Filter').innerHTML=string;
-    const filter=document.getElementById("filter");
+    for (let i = 0; i < array.length; i++) {
+        let eleccion = array[i];
 
-    filter.addEventListener('change',()=>{
-     condition=filter.value;
-     const pokedexFiltrado=pokemon.filterData(data,condition);
-     document.getElementById('list-pokemon').innerHTML=crearPlantilla(pokedexFiltrado);
-     })
-    
+        string = string + '<option values="' + eleccion + '">' + eleccion + '</option>';
+    }
+    string = '<select id="filter">' + string + '</select>';
+    document.getElementById('Filter').innerHTML = string;
+    const filter = document.getElementById("filter");
+
+    filter.addEventListener('change', () => {
+        condition = filter.value;
+        if (condition === 'Evolucion1' || condition === 'Evolucion2' || condition === 'Evolucion3') {
+            const filterEvolucion = pokemon.NivelEvolu(condition);
+            document.getElementById('list-pokemon').innerHTML = crearPlantilla(filterEvolucion)
+
+        } else {
+            const pokedexFiltrado = pokemon.filterData(data, condition);
+            document.getElementById('list-pokemon').innerHTML = crearPlantilla(pokedexFiltrado);
+        }
+    })
+
 };
 // CALCULAR DATOS DE EVOLUCION 
 
 const btnCalculate = document.getElementById('btn-calculate');
-btnCalculate.addEventListener('click',()=>{
+btnCalculate.addEventListener('click', () => {
     const namePokemon = document.getElementById('name-pokemon').value;
     const candyCount = document.getElementById('candy_count').value;
 
-    const calculateEvolution = document.getElementById('calculate-evolution'); 
-    const evolutionResult = document.getElementById('evolution-result'); 
+    const calculateEvolution = document.getElementById('calculate-evolution');
+    const evolutionResult = document.getElementById('evolution-result');
 
-    const resultado = pokemon.computeStats(data,namePokemon,candyCount);
+    const resultado = pokemon.computeStats(data, namePokemon, candyCount);
     console.log(resultado);
 
-    const plantillaResultado = (data)=>{
-        for(let i = 0; i< data.length ;i++){
-            respuesta =`
+    const plantillaResultado = (data) => {
+        for (let i = 0; i < data.length; i++) {
+            respuesta = `
                 <p>${data[i].evolution}</>
                 <p>${data[i].candy_evolution}</>
             `;
@@ -130,10 +136,7 @@ btnCalculate.addEventListener('click',()=>{
     }
     calculateEvolution.style.display = 'none';
     evolutionResult.style.display = 'block';
-    evolutionResult.innerHTML= plantillaResultado(resultado);
-  
-    
-
+    evolutionResult.innerHTML = plantillaResultado(resultado);
 });
 
 
@@ -156,7 +159,7 @@ filterPokemon.addEventListener('change',()=>{
  });
 
 //aqui ira lo de dibujar
- 
+
 const filterPokemon = document.getElementById('filter-pokemon');
 const generarTipo = (listTipos)=>{
     let types =[`<option disabled selected>Filtrar por:</option>`];
