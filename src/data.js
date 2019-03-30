13aeaa5f570d74e2eeedd2252a6e34dc696c94c4
LeslieZarate <fiorelly30zarate@gmail.com>
2019-03-29 23:06:34 -0500

    avance de test y correccion de errores

--- src/data.js
+++ src/data.js
@@ -1,55 +1,55 @@
-/**** MOSTRAR POEKOM ****/
+/* *** MOSTRAR POEKOM ****/
 const mostrarListaPokemon = (dataPoke) => {
-  const pokedex = dataPoke.map(data =>{
-  return{num : data.num , img : data.img , name : data.name , avg_spawns : data.avg_spawns};
+  const pokedex = dataPoke.map(data => {
+    return { num: data.num, img: data.img, name: data.name, avgSpawns: data.avg_spawns };
   });
   return pokedex;
-};/*
-/**** ORDENAR POKEMON ****/
-const sortData = (data,sortBy,sortOrder) => {
-  let listaOrdenada;    
-  switch(sortOrder){
-    case 'ascendente':
-      if(sortBy ==='avg_spawns'){
-        listaOrdenada = data.sort((a,b)=> a.avg_spawns - b.avg_spawns);
-      }
-      else if(sortBy ==='az' ){
-        listaOrdenada = data.sort((a,b)=> a.name > b.name ? 1:-1);
-      }            
+};
+/* *** ORDENAR POKEMON ****/
+const sortData = (data, sortBy, sortOrder) => {
+  let listaOrdenada;
+  switch (sortOrder) {
+  case 'ascendente':
+    if (sortBy === 'avg_spawns') {
+      listaOrdenada = data.sort((eleA, eleB) => eleA.avgSpawns - eleB.avgSpawns);
+    } else {
+      listaOrdenada = data.sort((eleA, eleB) => eleA.name > eleB.name ? 1 : -1);
+    }
     break;
-    case 'descendente':
-      listaOrdenada = data.sort((a,b)=>b.name > a.name ? 1:-1);
+  case 'descendente':
+    if (sortBy === 'za') {
+      listaOrdenada = data.sort((eleA, eleB) => eleB.name > eleA.name ? 1 : -1);
+    }    
     break;
   }
   return listaOrdenada;
 };
-/***  FILTRAR POKEMON ***/
-//1.Funcion propia 
-const filterData = (data,condition) =>{
-  let listaFiltrada = data.filter((element)=>{
-    for(let i = 0 ; i < element.type.length ; i++){
-      if (element.type[i] === condition){
+/* ****FILTRAR POKEMON ***/
+// 1.Funcion propia 
+const filterData = (data, condition) => {
+  let listaFiltrada = data.filter((element) => {
+    for (let i = 0; i < element.type.length; i++) {
+      if (element.type[i] === condition) {
         return 1;
       }
     }
   });
   return listaFiltrada;
 };
-
-//2.Funcion de obtener solo el array de todos los tipos 
-const listType = (data) =>{
-  const arrTipos =[];
-  let tipo =[];
+// 2.Funcion de obtener solo el array de todos los tipos 
+const listType = (data) => {
+  const arrTipos = [];
+  let tipo = [];
   data.forEach(element => {
-    for(let i = 0 ; i < element.type.length ; i++){
+    for (let i = 0; i < element.type.length; i++) {
       arrTipos.push(element.type[i]);
-    }  
-    tipo = [...new Set(arrTipos)]
-    });
+    }
+    tipo = [...new Set(arrTipos)];
+  });
   return tipo;
 };
-//3.Funcion para tipos 
-const NivelEvolution = (data,condicion) => {
+// 3.Funcion para tipos 
+const nivelEvolution = (data, condicion) => {
   let Evolucion1 = [];
   let Evolucion2 = [];
   let Evolucion3 = [];
@@ -57,88 +57,84 @@ const NivelEvolution = (data,condicion) => {
     let element = data[i];
     if (element.prev_evolution === undefined) {
       Evolucion1.push(element);
-    }
-    else {
-      let cantidadPrev = element.prev_evolution.length
+    } else {
+      let cantidadPrev = element.prev_evolution.length;
       switch (cantidadPrev) {
-        case 1:
-          Evolucion2.push(element); break;
-        case 2:
-          Evolucion3.push(element); break;
+      case 1:
+        Evolucion2.push(element); break;
+      case 2:
+        Evolucion3.push(element); break;
       }
     }
   }
   switch (condicion) {
-    case "Evolucion1": return Evolucion1; 
-    case "Evolucion2": return Evolucion2; 
-    case "Evolucion3": return Evolucion3; 
+  case 'Evolucion1': return Evolucion1;
+  case 'Evolucion2': return Evolucion2;
+  case 'Evolucion3': return Evolucion3;
   }
 };
-
-/***** CALCULAR POKEMON ******/
-//1. Obtener una data que se ajuste a  la medida de el calculo para la evolucion 
-const dataListPokemon = (dataPoke) =>{
-  const dataCalculate =[];
+/* **** CALCULAR POKEMON ******/
+// 1. Obtener una data que se ajuste a  la medida de el calculo para la evolucion 
+const dataListPokemon = (dataPoke) => {
+  const dataCalculate = [];
   let propiedad;
-  for(let i = 0 ; i < dataPoke.length ; i++  ){
+  for (let i = 0; i < dataPoke.length; i++) {
     propiedad = dataPoke[i].hasOwnProperty('next_evolution');
-    if(propiedad === true ){
-       propiedad = dataPoke[i].next_evolution.length;
-    } 
-    else{
+    if (propiedad === true) {
+      propiedad = dataPoke[i].next_evolution.length;
+    } else {
       propiedad = 0;
-    }      
-    dataCalculate.push( {
-       num : dataPoke[i].num , 
-       name : dataPoke[i].name , 
-       candy_count : dataPoke[i].candy_count ,
-       next_evolution : dataPoke[i].next_evolution,
-       num_evolutions : propiedad,
-       img : dataPoke[i].img })
+    }
+    dataCalculate.push({
+      num: dataPoke[i].num,
+      name: dataPoke[i].name,
+      candyCount: dataPoke[i].candy_count,
+      nextEvolution: dataPoke[i].next_evolution,
+      numEvolutions: propiedad,
+      img: dataPoke[i].img
+    });
   }
   return dataCalculate;
 };
-//console.log(dataListPokemon(data));
-//2. funcion de calculo de evolucion
-const computeStats = (dataPoke,pokemon,candyInput) =>{
-  const  dataCalculatePokemon = dataListPokemon(dataPoke);
-  //buscando al pokemon
-  const  pokemonFind = dataCalculatePokemon.find(poke=>poke.name === pokemon);
-  const  numberEvolutions = pokemonFind.num_evolutions;    // saber el numero de evoluciones 
-  let arrResult = [];                                     // almacenar los datos de resultados 
-  let pokemonEvolution ;                                  // Evolucion de Pokemon
-  let calculoEvolution;                                  // Hallar dulces que falta 
-
-  if(numberEvolutions > 0){
-    pokemonEvolution = dataCalculatePokemon.find( poke => poke.name === pokemonFind.next_evolution[0].name );
-    calculoEvolution = Math.abs(pokemonFind.candy_count - parseInt(candyInput));  
-  }
-  else{
-    pokemonEvolution = pokemonFind
+// console.log(dataListPokemon(data));
+// 2. funcion de calculo de evolucion
+const computeStats = (dataPoke, pokemon, candyInput) => {
+  const dataCalculatePokemon = dataListPokemon(dataPoke);
+  // buscando al pokemon
+  const pokemonFind = dataCalculatePokemon.find(poke => poke.name === pokemon);
+  // saber el numero de evoluciones 
+  const numberEvolutions = pokemonFind.numEvolutions;   
+  // almacenar los datos de resultados
+  let arrResult = [];                                      
+  // Evolucion de Pokemon
+  let pokemonEvolution;                                 
+  // Hallar dulces que falta
+  let calculoEvolution;                
+  if (numberEvolutions > 0) {
+    pokemonEvolution = dataCalculatePokemon.find(poke => poke.name === pokemonFind.nextEvolution[0].name);
+    calculoEvolution = Math.abs(pokemonFind.candyCount - parseInt(candyInput));
+  } else {
+    pokemonEvolution = pokemonFind;
     calculoEvolution = 0;
   }
-    arrResult.push({
-    pokemonFirstName : pokemonFind.name, 
-    pokemonFirstImg : pokemonFind.img,
-    pokemonFirstCandy:pokemonFind.candy_count,
-    pokemonFirstInput : candyInput,
-
-    pokemonEvolutionName :pokemonEvolution.name, 
-    pokemonEvolutionImg :pokemonEvolution.img,
-    candy_evolution : calculoEvolution
-    });    
-
-    return arrResult;
+  arrResult.push({
+    pokemonFirstName: pokemonFind.name,
+    pokemonFirstImg: pokemonFind.img,
+    pokemonFirstCandy: pokemonFind.candyCount,
+    pokemonFirstInput: candyInput,
+    pokemonEvolutionName: pokemonEvolution.name,
+    pokemonEvolutionImg: pokemonEvolution.img,
+    candyEvolution: calculoEvolution
+  });
+  return arrResult;
 };
-
- // console.log(computeStats(data ,"Venusaur",101)); 
+// console.log(computeStats(data ,"Venusaur",101)); 
 window.pokemon = {
   mostrarListaPokemon: mostrarListaPokemon,
-  sortData : sortData,
-  filterData : filterData,
-  listType : listType,
-  NivelEvolution: NivelEvolution,
-  dataListPokemon :dataListPokemon,
-  computeStats :computeStats,
-  
-}
+  sortData: sortData,
+  filterData: filterData,
+  listType: listType,
+  nivelEvolution: nivelEvolution,
+  dataListPokemon: dataListPokemon,
+  computeStats: computeStats,
+};
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,44 +1,54 @@
 global.window = global;
-global.assert = require('chai').assert;
+// global.assert = require('chai').assert;
 require('../src/data');
-require('./data.spec.js');
+// require('./data.spec.js');
 
 const data = [
-  {id: 1,  num: "001", name:'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png',avg_spawns : 69,  egg: "2 km" , type:["Grass", "Poison"]  },
-  {id: 79, num: "079", name: "Slowpoke", img: "http://www.serebii.net/pokemongo/pokemon/079.png",avg_spawns : 105, egg: "5 km",  type:["Water", "Psychic"] },
-  {id: 150, num: "150", name: "Mewtwo", img: "http://www.serebii.net/pokemongo/pokemon/150.png", avg_spawns : 0,   egg: "Not in Eggs", type: ["Psychic"]  }
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
 ];
 
-const orderDataAsc =[
-  {id: 150, num: "150", name: "Mewtwo", img: "http://www.serebii.net/pokemongo/pokemon/150.png", avg_spawns : 0, egg: "Not in Eggs", type: ["Psychic"]},
-  {id: 1,   num: "001", name:'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png',avg_spawns : 69,  egg: "2 km" , type:["Grass", "Poison"] },
-  {id: 79,  num: "079", name: "Slowpoke", img: "http://www.serebii.net/pokemongo/pokemon/079.png",avg_spawns : 105, egg: "5 km" ,type:["Water", "Psychic"] }  
-];
-const sortBySpawns = 'avg_spawns';
+const avgSpawns = 'avg_spawns';
 const sortOrderAsc = 'ascendente';
 
-const orderDataDesc = [
-  {id: 79,  num: "079", name: "Slowpoke", img: "http://www.serebii.net/pokemongo/pokemon/079.png",avg_spawns : 105, egg: "5 km",type:["Water", "Psychic"]},
-  {id: 150, num: "150", name: "Mewtwo", img: "http://www.serebii.net/pokemongo/pokemon/150.png", avg_spawns : 0, egg: "Not in Eggs", type: ["Psychic"]},
-  {id: 1,   num: "001", name:'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png',avg_spawns : 69,  egg: "2 km",type:["Grass", "Poison"]} 
+const orderDataAsc1 = [
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
 ];
-const sortByName = 'za';
+
+const name1 = 'az';
+
+const orderDataAsc2 = [
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
+];
+
+const name2 = 'za';
 const sortOrderDesc = 'descendente';
 
-const filterDataType = [
-  {id: 79, num: "079", name: "Slowpoke", img: "http://www.serebii.net/pokemongo/pokemon/079.png",avg_spawns : 105, egg: "5 km",  type:["Water", "Psychic"] },
-  {id: 150, num: "150", name: "Mewtwo", img: "http://www.serebii.net/pokemongo/pokemon/150.png", avg_spawns : 0,   egg: "Not in Eggs", type: ["Psychic"]  }
+const orderDataDesc = [
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] }
 ];
-const condition = "Psychic";
-const listTypeData = ["Water", "Psychic","Grass", "Poison"]
+
+const filterDataType = [
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
+];
+const condition = 'Psychic';
+const listTypeData = ['Water', 'Psychic', 'Grass', 'Poison'];
 
 
-describe('pokemon', () => {  
+describe('pokemon', () => {
   test('debería ser un object', () => {
     expect(typeof pokemon).toBe('object');
   });
-  
-  describe("Mostrar ciertas propiedades" , ()=>{
+
+  describe('Mostrar ciertas propiedades', () => {
     test('mostrarListaPokemon  deberia ser una funcion', () => {
       expect(typeof pokemon.mostrarListaPokemon).toBe('function');
     });
@@ -48,29 +58,32 @@ describe('pokemon', () => {
   });
 
 
-  describe("Ordenar Pokemones" , ()=>{
+  describe('Ordenar Pokemones', () => {
     test('sortData  deberia ser una funcion', () => {
-      expect(typeof pokemon.sortData ).toBe('function');
+      expect(typeof pokemon.sortData).toBe('function');
     });
     test('debería retornar un nuevo array ordenado ascendentemente por Apariciones', () => {
-      expect(pokemon.sortData(data,sortBySpawns ,sortOrderAsc)).toEqual(orderDataAsc);
+      expect(pokemon.sortData(data, avgSpawns, sortOrderAsc)).toEqual(orderDataAsc1);
+    });
+    test('debería retornar un nuevo array ordenado ascendentemente por nombre', () => {
+      expect(pokemon.sortData(data, name1, sortOrderAsc)).toEqual(orderDataAsc2);
     });
     test('debería retornar un nuevo array ordenado descendentemente por nombre', () => {
-      expect(pokemon.sortData(data,sortByName,sortOrderDesc)).toEqual(orderDataDesc);
-    });    
+      expect(pokemon.sortData(data, name2, sortOrderDesc)).toEqual(orderDataDesc);
+    });
   });
 
-  
-  describe("Filtrar Pokemones por tipo " , ()=>{
+
+  describe('Filtrar Pokemones por tipo ', () => {
     test('filterData  deberia ser una funcion', () => {
       expect(typeof pokemon.filterData).toBe('function');
     });
     test('debería retornar un nuevo array filtrado por tipo "Psychic" ', () => {
-      expect(pokemon.filterData(data,condition)).toEqual(filterDataType);
+      expect(pokemon.filterData(data, condition)).toEqual(filterDataType);
     });
   });
 
-  describe(" Obtener todos solo los tipos de pokemones " , ()=>{
+  describe(' Obtener todos solo los tipos de pokemones ', () => {
     test(' listType deberia ser una funcion', () => {
       expect(typeof pokemon.listType).toBe('function');
     });
@@ -78,33 +91,4 @@ describe('pokemon', () => {
       expect(pokemon.listType(data)).toEqual(listTypeData);
     });
   });
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
