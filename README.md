89acd4e3b8d65e76e544415001e7fca21a89e780
LeslieZarate <fiorelly30zarate@gmail.com>
2019-03-31 21:51:10 -0500

    Cambio del readmi

--- README.md
+++ README.md
@@ -1,64 +1,80 @@
-# Data Lovers 
-# POKEMON
-Pokemon Go se crea en base a la necesidad de poder ayudar a los usuarios amantes del videojuego y poder conocer a mayor detalle a sus pokemones, asi  mismo les permite conocer a los pokemones por tipo, evolucion y poder calcular cuantos caramelos les falta para su siguiente evolucion.
+# POKEMON 
+#Introducción
+Pokemon Go es un videojuego de aventura de realidad aumentada, creado por Niantic, Inc. para dispositivos iOS y Android.Desde el día de su lanzamiento a causado furor entre los jóvenes y adultos.De acuerdo con el concepto original, los entrenadores buscan capturar Pokémon hasta completar una colección. El otro objetivo es adiestrarlos para que ganen batallas frente a otras criaturas. El juego ha conseguido superar los 65 millones de usuarios al día y ha generado aglomeraciones en las ciudades. Pero, ¿por qué este juego ha causado tanto impacto en los usuarios? La idea de llegar a convertirse en un maestro Pokémon es un feeling con el que ha crecido toda una generación, Pokémon Go te permite vivir la experiencia de un verdadero entrenador pokémon.
 
-![PokemonGo](src/img/imgSlider/Slider3.jpg)
+# Pokemon - Data Lovers 
+Es una herramienta de ayuda para todos los usuarios de Pokémon Go, ya que te permite hacer, desde una búsqueda específica de un Pokémon, filtrarlos por tipo y evolución. Hacer un cálculo real de caramelos que le falta a tu Pokémon para llegar a la siguiente evolución.
+
+![PokemonGo](src/img/Slider3.jpg)
+
+##Definición del Producto
+**¿Quiénes son los principales usuarios de producto?**
+Los principales usuarios son los jugadores tanto nuevos como antiguos de Pokémon Go.
+
+**¿Cuáles son los objetivos de estos usuarios en relación con tu producto?**
+Buscar Pokémon en específico y conocer datos sobre ellos 
+
+**¿Cómo crees que el producto que estás creando está resolviendo sus problemas?**
+Nuestro producto facilíta la búsqueda de pokemon ,ademas ayuda a que estos puedan saber cuantos dulces le falta al pokemon para su siguiente evolucion.
+
 ## Historias de Usuarios
+Con el objetivo de conocer las verdaderas necesidades de los usuarios que estan inciando en el juego de  POKEMON GO decidimos realizar una pequeña encuesta que fue de gran utilidad para conocer los primeros pasos a dar antes de desarrollar el producto.Encontramos que nuestros usuarios necesitaban lo sieguiente puntos basicos :
 
-Con el objetivo de conocer las verdaderas necesidades de los usuarios que son amantes de POKEMON
-GO decidimos realizar una pequeña encuesta que fue de gran utilidad para conocer los primeros pasos
-a dar antes de desarrollar el producto.
+Mostrar Data: Según usuarios desean que se muestre por defecto en la web al ingresar solo información relevante como el numero de pokedex ,la imagen y el nombre, para que sea más ordenada y simple de usar.
 
-nos permitio crear un prototipo de baja fidelidad en base a las necesidades recolectadas por medio
-de las encuestas.
+Filtrar Data: Los usuarios indicaron que les gustaría un filtro por tipo de pokemon y tambien por evolcion ,ya que al conocer esta información les sería útil antes de empezar a jugar Pokemon GO.
 
-Gracias a los prototipos pudimos crear las historias de usuarios y definir pequeñas partes del proyecto, basando en los criterios y herramientas que deseamos implementar y asi mismo tener mejor organizar y planificaion del proyecto.
+Ordenar Data: Usuarios desean que se les permita ordenar la data de manera alfabética ascendente(A-Z) y alfabética descendente(Z-A) ,y tambie por el mayor numero de apariciones  ya que les ayudaría a encontrar más rápido el pokemón que les interesa.
 
-### Diseño de la Interfaz de Usuario
+Calcular Data:Los jugadores de Pokémon GO indicaron que les ayudaría en sus decisiones tener el calculo de cuantos duceles le falta a un pokemon para su evolucion
 
-#### Prototipo de baja fidelidad
+##Encuestas 
+![pregunta1](src/img/progunta1.png)
+![pregunta1](src/img/progunta2.png)
+![pregunta1](src/img/progunta3.png)
+![pregunta1](src/img/progunta4.png)
+![pregunta1](src/img/progunta5.png)
 
-Durante tu trabajo deberás haber hecho e iterado sketches (boceto) de tu
-solución usando papel y lápiz. Te recomendamos tomar fotos de todas las
-iteraciones que hagas, las subas a tu repositorio, y las menciones en tu
-`README.md`.
+## Diseño de la Interfaz de Usuario
+En funcion a las necesidades nuestros usuario indicaron en la encusta se desarrollo un prototipo de baja fidelidad y un prototipo de alta fidelidad.
 
-#### Prototipo de alta fidelidad
+### Prototipo de baja fidelidad
+Se realizó siguiendo como pauta las historias de usuario que resultaron de las encuestas a los jugadores de Pokemón GO .
+####Sketch1
+![Sketch1](src/img/sketch1.png)
+####Sketch2
+![Sketch2](src/img/sketch2.png)
+####Sketch3
+![Sketch3](src/img/sketch3.png)
+
+### Prototipo de alta fidelidad
 para el prototipo de alta fidelidad desidimos usar Adobe XD que es una herramienta que nos parecio mas amigable y nos permitio exportar el diseño a Zeplin
-![prot1](imgReadme/prot1)
-![prot2](imgReadme/prot2)
-![prot3](imgReadme/prot3)
-![prot4](imgReadme/prot4)
-![prot5](imgReadme/prot5)
-![prot6](imgReadme/prot6)
-![prot7](imgReadme/prot7)
+![prot1](src/img/prot1.png)
+![prot2](src/img/prot2.png)
+![prot3](src/img/prot3.png)
+![prot4](src/img/prot4.png)
+![prot5](src/img/prot5.png)
+![prot6](src/imgprot6.png)
+![prot7](src/img/prot7.png)
 
 Diseño en Zeplin [Enlace Zeplin](https://zpl.io/bLP84JB)
 
 POkEMON GO es una pagina web que se centra en otorgarle al los jugadores y conocedores del juego puedan informarse acerca de sus pokemones segun su tipo etapa de evolucion.
 ### PRIMERA VISTA
-![VIEW1](imgReadme/view1)
+![VIEW1](src/img/view1.png)
 ### SEGUNDA VISTA
-![VIEW2](imgReadme/view2)
+![VIEW2](src/img/view2.png)
 ### TERCERA VISTA
-![VIEW3](imgReadme/view3)
+![VIEW3](src/img/view3.png)
 ### CUARTA VISTA
-![VIEW4](imgReadme/view4)
+![VIEW4](src/img/view4.png)
 ### QUINTA VISTA
-![VIEW5](imgReadme/view5)
+![VIEW5](src/img/view5.png)
 ### SEXTA VISTA
-![VIEW6](imgReadme/view6)
+![VIEW6](src/img/view6.png)
 ### SEPTIMA VISTA
-![VIEW7](imgReadme/view7)
+![VIEW7](src/img/view7.png)
 
 
-
-Principalmente uno de los objetivos es darle la informacion mas relevante a los usuarios y que puedan resolver cualquier duda que se tenga sobre sus pokemones o si desean conocer mas sobre ellos atarves de la web que es amigable al usuario.
-
-- Cómo crees que el producto que estás creando está resolviendo sus problemas.
-
-Principalmente resuelve el tema relacionado a la poca informacion que pueden tener los jugadores acerca de los pokemones y no solo eso si no tambien a los aficionados a la serie de Pokemon.
-
-Tener informacion mas detallada sobre los pokemones que capturan lograra que se interecen mas en capturarlos y creara una gran expectativa cuando se busque evolucionar a algun pokemon ya que la web te permitira saber cuantos caramelos le falta a tu pokemon lograr la siguiente evolucion. 
 
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -3,53 +3,94 @@ global.window = global;
 require('../src/data');
 // require('./data.spec.js');
 
-
-
-//  describe("NivelEvolution" , ()=>{
-//   test('NivelEvolution deberia ser una funcion', () => {
-//     expect(typeof pokemon.NivelEvolution).toBe('function');
-//   });
-
-// });
-
 const data = [
-  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
-  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
-  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avg_spawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avg_spawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avg_spawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
 ];
 
 const avgSpawns = 'avg_spawns';
 const sortOrderAsc = 'ascendente';
-
 const orderDataAsc1 = [
-  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
-  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
-  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avg_spawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avg_spawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avg_spawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
 ];
 
 const name1 = 'az';
-
 const orderDataAsc2 = [
-  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
-  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
-  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avg_spawns: 69, egg: '2 km', type: ['Grass', 'Poison'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avg_spawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avg_spawns: 105, egg: '5 km', type: ['Water', 'Psychic'] }
 ];
 
 const name2 = 'za';
 const sortOrderDesc = 'descendente';
-
 const orderDataDesc = [
-  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
-  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
-  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avgSpawns: 69, egg: '2 km', type: ['Grass', 'Poison'] }
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avg_spawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avg_spawns: 0, egg: 'Not in Eggs', type: ['Psychic'] },
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', avg_spawns: 69, egg: '2 km', type: ['Grass', 'Poison'] }
 ];
 
-const filterDataType = [
-  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avgSpawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
-  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avgSpawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
-];
 const condition = 'Psychic';
+const filterDataType = [
+  { id: 79, num: '079', name: 'Slowpoke', img: 'http://www.serebii.net/pokemongo/pokemon/079.png', avg_spawns: 105, egg: '5 km', type: ['Water', 'Psychic'] },
+  { id: 150, num: '150', name: 'Mewtwo', img: 'http://www.serebii.net/pokemongo/pokemon/150.png', avg_spawns: 0, egg: 'Not in Eggs', type: ['Psychic'] }
+];
+
 const listTypeData = ['Water', 'Psychic', 'Grass', 'Poison'];
+
+const data2 = [
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png'},
+  { id: 78, num: '078', name: 'Rapidash', img: 'http://www.serebii.net/pokemongo/pokemon/078.png', prev_evolution: [{num: '077', name: 'Ponyta'}]},
+  { id: 3, num: '003', name: 'Venusaur', img: 'http://www.serebii.net/pokemongo/pokemon/003.png', prev_evolution: [{num: '001', name: 'Bulbasaur'}, {num: '002', name: 'Ivysaur'}]}
+];
+const typeEvolution1 = 'Evolucion1';
+const listEvolution1 = [{ id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png'}];
+const typeEvolution2 = 'Evolucion2';
+const listEvolution2 = [{ id: 78, num: '078', name: 'Rapidash', img: 'http://www.serebii.net/pokemongo/pokemon/078.png', prev_evolution: [{num: '077', name: 'Ponyta'}]}];
+const typeEvolution3 = 'Evolucion3';
+const listEvolution3 = [
+  { id: 3, num: '003', name: 'Venusaur', img: 'http://www.serebii.net/pokemongo/pokemon/003.png', prev_evolution: [{num: '001', name: 'Bulbasaur'}, {num: '002', name: 'Ivysaur'}]}
+];
+
+
+const data3 = [ 
+  { id: 1, num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', candy_count: 25, next_evolution: [{num: '002', name: 'Ivysaur'}, {num: '003', name: 'Venusaur'}]},
+  { id: 2, num: '002', name: 'Ivysaur', img: 'http://www.serebii.net/pokemongo/pokemon/002.png', candy_count: 100, next_evolution: [{num: '003', name: 'Venusaur'}]},
+  { id: 3, num: '003', name: 'Venusaur', img: 'http://www.serebii.net/pokemongo/pokemon/003.png', }
+];
+const resultA = [
+  { num: '001', name: 'Bulbasaur', img: 'http://www.serebii.net/pokemongo/pokemon/001.png', candy_count: 25, next_evolution: [{num: '002', name: 'Ivysaur'}, {num: '003', name: 'Venusaur'}], num_evolutions: 2},
+  { num: '002', name: 'Ivysaur', img: 'http://www.serebii.net/pokemongo/pokemon/002.png', candy_count: 100, next_evolution: [{num: '003', name: 'Venusaur'}], num_evolutions: 1},
+  { num: '003', name: 'Venusaur', img: 'http://www.serebii.net/pokemongo/pokemon/003.png', candy_count: undefined, next_evolution: undefined, num_evolutions: 0}
+];
+
+const poke = 'Ivysaur';
+const candyInput = 33;
+const resultEvol1 = [
+  {
+    pokemonFirstName: 'Ivysaur', 
+    pokemonFirstImg: 'http://www.serebii.net/pokemongo/pokemon/002.png', 
+    pokemonFirstCandy: 100, 
+    pokemonFirstCandyInput: 33,
+    pokemonEvolutionName: 'Venusaur', 
+    pokemonEvolutionImg: 'http://www.serebii.net/pokemongo/pokemon/003.png',
+    candyEvolution: 67
+  }
+];
+const poke2 = 'Venusaur';
+const resultEvol2 = [
+  {
+    pokemonFirstName: 'Venusaur', 
+    pokemonFirstImg: 'http://www.serebii.net/pokemongo/pokemon/003.png', 
+    pokemonFirstCandy: undefined, 
+    pokemonFirstCandyInput: 33,
+    pokemonEvolutionName: 'Venusaur', 
+    pokemonEvolutionImg: 'http://www.serebii.net/pokemongo/pokemon/003.png',
+    candyEvolution: 0
+  }
+];
 
 
 describe('pokemon', () => {
@@ -66,7 +107,6 @@ describe('pokemon', () => {
     });
   });
 
-
   describe('Ordenar Pokemones', () => {
     test('sortData  deberia ser una funcion', () => {
       expect(typeof pokemon.sortData).toBe('function');
@@ -81,7 +121,6 @@ describe('pokemon', () => {
       expect(pokemon.sortData(data, name2, sortOrderDesc)).toEqual(orderDataDesc);
     });
   });
-
 
   describe('Filtrar Pokemones por tipo ', () => {
     test('filterData  deberia ser una funcion', () => {
@@ -98,6 +137,41 @@ describe('pokemon', () => {
     });
     test('deberia retornar un nuevo arrar con solo los tipos de pokemon', () => {
       expect(pokemon.listType(data)).toEqual(listTypeData);
+    });
+  });
+
+  describe(' Filtrar por Evolucion', () => {
+    test(' nivelEvolution deberia ser una funcion', () => {
+      expect(typeof pokemon.nivelEvolution).toBe('function');
+    });
+    test('deberia retornar un nuevo array con todos los pokemon de primera evolucion', () => {
+      expect(pokemon.nivelEvolution(data2, typeEvolution1)).toEqual(listEvolution1);
+    });
+    test('deberia retornar un nuevo array con todos los pokemon de segunda evolucion', () => {
+      expect(pokemon.nivelEvolution(data2, typeEvolution2)).toEqual(listEvolution2);
+    });
+    test('deberia retornar un nuevo array con todos los pokemon de tercera evolucion', () => {
+      expect(pokemon.nivelEvolution(data2, typeEvolution3)).toEqual(listEvolution3);
+    });
+  });
+
+  describe('Mostrar nuemro de evoluciones ', () => {
+    test('dataListPokemon deberia ser una funcion ', ()=>{
+      expect(typeof pokemon.dataListPokemon).toBe('function');
+    });
+    test('deebria retornar un nuevo array con el numero de evolcuiones  ', ()=>{
+      expect(pokemon.dataListPokemon(data3)).toEqual(resultA);
+    });
+  });
+  describe('Mostrar calculo de evolucion ', () => {
+    test('computeStats deberia ser una funcion ', ()=>{
+      expect(typeof pokemon.computeStats).toBe('function');
+    });
+    test('deebria retornar un nuevo array con el numero de evolcuiones  ', ()=>{
+      expect(pokemon.computeStats(data3, poke, candyInput)).toEqual(resultEvol1);
+    });
+    test('deebria retornar un nuevo array con el numero de evolcuiones  ', ()=>{
+      expect(pokemon.computeStats(data3, poke2, candyInput)).toEqual(resultEvol2);
     });
   });
 });
